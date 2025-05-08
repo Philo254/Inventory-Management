80a187d5d9ceb17758ddb2bafa3910c996e8c6b1
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-05-08 09:21:44 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,11 @@ use App\Http\Controllers\UserController;
 /**Route::get('/', function () {
     return view('welcome');
 });**/
+
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
 
 
 Route::get('/', function () {
