6d639e7225048e73c1bebceeb99d174b57d6417f
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-05-13 11:11:10 +0300

    Add files via upload

---
+++ database/migrations/2025_05_13_075712_create_vendors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('CP')->comment('contact person');
+            $table->string('phone');
+            $table->integer('active')->comment('0=inactive,1=active,2=delete');
+
+            $table->integer('user_modified');
+
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
