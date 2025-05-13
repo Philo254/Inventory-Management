df864ac330df67f88c1e718c3f6acb86fc2e0ecc
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-05-13 16:49:28 +0300

    Add files via upload

---
+++ resources/views/Vendor/create.blade.php
@@ -0,0 +1,88 @@
+@extends('backend.app')
+@section('content')
+
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Vendor</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="">Home</a></li>
+                    <li class="breadcrumb-item ">Master</li>
+
+                    <li class="breadcrumb-item active">Create Vendor</li>
+                </ol>
+            </div>
+            </div>
+            </div>
+            </div>
+
+<!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Create Vendor</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form class="form-horizontal">
+                @csrf
+                <div class="card-body">
+                  <div class="form-group row">
+                    <div class="col-md-4">
+                     <label for="name" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="name" placeholder="Name">
+                    </div>
+                    </div>
+                    <div class="col-md-4">
+                     <label for="address" class="col-sm-2 col-form-label">Address</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="address" placeholder="Address">
+                    </div>
+                    </div>
+
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <div class="offset-sm-2 col-sm-10">
+                      <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                        <label class="form-check-label" for="exampleCheck2">Remember me</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-info">Sign in</button>
+                  <button type="submit" class="btn btn-default float-right">Cancel</button>
+                </div>
+                <!-- /.card-footer -->
+              </form>
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!--/.col (left) -->
+
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+
+            @endsection
--- resources/views/Vendor/index.blade.php
+++ resources/views/Vendor/index.blade.php
@@ -1,1 +1,22 @@
+@extends('backend.app')
+@section('content')
 
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Vendor</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="">Home</a></li>
+                    <li class="breadcrumb-item ">Master</li>
+
+                    <li class="breadcrumb-item active">Vendor</li>
+                </ol>
+            </div>
+            </div>
+            </div>
+            </div>
+
+            @endsection
