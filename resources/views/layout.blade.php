d976b3add545a9d2cb073926cdf635fe90e1ee98
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-05-15 12:09:02 +0300

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -79,7 +79,10 @@
         <div class="col-md-12">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><h2>INVENTORY SYSTEM</h2></a>
-                        <a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
+                        <div style="padding-left: 750px;">
+                            <a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
+                             </div>
+
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
