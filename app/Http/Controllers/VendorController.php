7ed88ff597af1796470f9169218e68d3c1c9279c
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-05-29 10:03:15 +0300

    Add files via upload

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\vendor;
+use Illuminate\View\View;
+use Yajra\DataTables\DataTables;
 
 class VendorController extends Controller
 {
@@ -11,7 +16,10 @@ class VendorController extends Controller
      */
     public function index()
     {
-        //
+         {
+            $vendors = vendor::all();
+            return view ('vendors.index')->with('vendors', $vendors);
+        }
     }
 
     /**
@@ -60,5 +68,31 @@ class VendorController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function datatable(){
+
+        $data = Vendor::all();
+        return Datatables::of($data)
+        ->addColumn('action', function ($data) {
+          $url_edit = url('vendor/'.$data->id.'/edit');
+          $url = url('vendor/'.$data->id);
+          $view = "<a class='btn btn-action btn-primary' href='".$url."' title='view'><i class='nav-icon fas fa-eye'></i></a>" ;
+          $edit = "<a class='btn btn-action btn-warning' href='".$url_edit."' title='Edit'><i class='nav-icon fas fa-edit'></i></a>" ;
+          $delete = "<button data-url= '".$url."' onclick='deleteData(this)' class ='btn btn-action btn-danger' title='Delete'><i class='nav-icon fas fa-trash-alt'></i></button>";
+
+          return $view."".$edit."".$delete;
+        })
+        ->editColumn('address',function ($data){
+            return str_ireplace(search:"\r\n", replace:',', $data->address );
+        })
+
+        ->editColumn('phone',function ($data){
+            return str_ireplace(search:"\r\n", replace:',', $data->phone );
+        })
+        -> rawColumns (['action'])
+        ->editColumn ('id', 'ID:{{$id}}')
+        ->make(true);
+
     }
 }
