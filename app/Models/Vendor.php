30ebb3067b0975b11e77a6c588b2fcefd09b017c
Philo254 <92971156+Philo254@users.noreply.github.com>
2025-06-18 12:58:45 +0300

    Add files via upload

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,5 +8,5 @@ class Vendor extends Model
 {
     protected $table = 'vendors';
     protected $primaryKey = 'id';
-    protected $fillable = ["name","Address","cp", "phone No","Active","User_modified",];
+    protected $fillable = ["name","address","cp", "phone","active","user_modified",];
 }
